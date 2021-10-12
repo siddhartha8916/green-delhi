0081e67c2c7e4b15bcfa645087c9d3cc537081f1
siddhartha8916 <siddhartha8916@gmail.com>
2021-10-12 07:05:10 +0530

    Second MMMM

--- djangoproject/pollution_project/pollution_register/views.py
+++ djangoproject/pollution_project/pollution_register/views.py
@@ -9,25 +9,6 @@ def air_quality_list(request):
     return render(request, "pollution_register/AQI_list.html", context)
 
 
-def air_quality_form(request, id=0):  # INSERT or UPDATE
-    if request.method == "GET":
-        if id==0:
-            form = AQI_form()
-        else:
-            aqi_element = air_quality.objects.get(pk=id)
-            form = AQI_form(instance=aqi_element)
-        return render(request, "pollution_register/AQI_form.html", {'form': form})
-    else:
-        if id==0:
-            form = AQI_form(request.POST)
-        else:
-            aqi_element = air_quality.objects.get(pk=id)
-            form = AQI_form(request.POST, instance=aqi_element)
-        if form.is_valid():
-            form.save()
-        return redirect('/air_quality/list')
-
-
 def air_quality_delete(request,id):
     aqi_element = air_quality.objects.get(pk=id)
     aqi_element.delete()
